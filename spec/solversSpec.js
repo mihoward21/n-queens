db33bfb70c01f9735ed1af051227c85297454c0f
darkstar21 <mikeh09@gmail.com>
2014-09-18 11:12:20 -0700

    Finished bitshifting

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -3,7 +3,7 @@ describe('solvers', function() {
 
   describe('findNRooksSolution()', function() {
 
-    it('finds a valid solution for n of 1-8', function() {
+    xit('finds a valid solution for n of 1-8', function() {
       _.range(1, 9).map(function(n) {
         var solutionBoard = new Board(findNRooksSolution(n));
 
@@ -16,7 +16,7 @@ describe('solvers', function() {
 
   describe('countNRooksSolutions()', function() {
 
-    it('finds the number of valid solutions for n of 1-8', function() {
+    xit('finds the number of valid solutions for n of 1-8', function() {
       _.range(1, 9).map(function(n) {
         var solutionCount = countNRooksSolutions(n);
         var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n];
@@ -29,7 +29,7 @@ describe('solvers', function() {
 
   describe('findNQueensSolution()', function() {
 
-    it('finds a valid solution for n of 0-8', function() {
+    xit('finds a valid solution for n of 0-8', function() {
       _.range(1, 8).map(function(n) {
         var solutionBoard = new Board(findNQueensSolution(n));
 
@@ -43,9 +43,9 @@ describe('solvers', function() {
   describe('countNQueensSolutions()', function() {
 
     it('finds the number of valid solutions for n of 0-12', function() {
-      _.range(0, 13).map(function(n) {
-        var solutionCount = countNQueensSolutions(n);
-        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200][n];
+      _.range(0, 16).map(function(n) {
+        var solutionCount = countNQueensBitShift(n);
+        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184][n];
 
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
